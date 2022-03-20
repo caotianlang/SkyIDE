00db40aa8b03df93d27570a2a0dfd7586a34b0f5
caotianlang <99237950+caotianlang@users.noreply.github.com>
2022-03-20 13:31:20 +0800

    Create Window

--- src/main.js
+++ src/main.js
@@ -1,2 +1,23 @@
-const {App} = require("electron")
+const {app,BrowerWindow} = require("electron")
+
+function createWindow(){
+    const win = new BrowerWindow({
+        width : 800,
+        height : 600
+    })
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(()=> {
+    createWindow()
+    
+    app.on('activate', function(){
+        if(BrowerWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function(){
+    if(process.platform !== 'darwin') app.quit()
+})
+
 

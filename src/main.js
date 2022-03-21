d9dc4c579b6ceef54ad7d78257b70759752ebf73
caotianlang <99237950+caotianlang@users.noreply.github.com>
2022-03-21 12:54:03 +0800

    Add tabs

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
 const {app,BrowserWindow} = require("electron")
+const TabGroup = require("electron-tabs")
 
 function createWindow(){
     const win = new BrowserWindow({
         width : 800,
-        height : 600
+        height : 600,
+        webPreferences: {
+            nodeIntegration: true,
+            webviewTag: true
+        }
     })
     win.loadFile('src/index.html')
 }
 
 app.whenReady().then(()=> {
     createWindow()
-    
+    let tabGroup = new TabGroup()
+    let tab = tabGroup.addTab({
+        title: "Start",
+        src: "http://electron.atom.io",
+        visible: true
+    })    
     app.on('activate', function(){
         if(BrowerWindow.getAllWindows().length === 0) createWindow()
     })
